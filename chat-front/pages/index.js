3a9e24f948ab059ea32b26b08437582ad748a736
Eomhyunjun <zzp1318@gmail.com>
2022-07-27 16:46:25 +0900

    front

---
+++ chat-front/pages/index.js
@@ -0,0 +1,69 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3000");
+
+export default function Home() {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  }, []);
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("message", { name, message });
+    setState({ message: "", name });
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}:<span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="card">
+      <form onSubmit={onMessageSubmit}>
+        <h1>Message</h1>
+        <div className="name-field">
+          <input
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.name}
+            label="Name"
+          />
+        </div>
+        <div>
+          <input
+            name="message"
+            onChange={(e) => onTextChange(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message"
+          />
+        </div>
+        <button>Send Message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Chat log</h1>
+        {renderChat()}
+      </div>
+    </div>
+  );
+}
